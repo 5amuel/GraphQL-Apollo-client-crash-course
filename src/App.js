81ab868d4fdd382ceb8639ceb690f68f278532cd
samuel <samuel.martinez.morales@gmail.com>
2021-11-08 17:13:14 -0600

    Lazy queries

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route, Link } from 'react-router';
 import './App.css';
 import CharactersList from './pages/CharactersList';
 import Character from './pages/Character';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={ <CharactersList /> } />
+        <Route path='/search' element={ <Search /> } />
         <Route path='/:id' element={ <Character /> } />
       </Routes>
     </div>
